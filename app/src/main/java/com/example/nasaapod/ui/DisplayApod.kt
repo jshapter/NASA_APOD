9b7ecd7558db335a76b3af544c5fbaaf3df290ea
jasha <j.a.shapter@gmail.com>
2023-08-01 05:05:11 +0100

    selectable text

--- app/src/main/java/com/example/nasaapod/ui/DisplayApod.kt
+++ app/src/main/java/com/example/nasaapod/ui/DisplayApod.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Copyright
 import androidx.compose.material.icons.filled.Download
@@ -25,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -45,9 +47,12 @@ fun DisplayApod(
     copyright: String,
     description: String
 ) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             CenterAlignedTopAppBar(
+                scrollBehavior = scrollBehavior,
                 colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.primary),
                 title = {
                     Text(
@@ -78,13 +83,17 @@ fun DisplayApod(
                                 verticalAlignment = Alignment.CenterVertically,
                                 modifier = Modifier.fillMaxWidth()
                             ) {
-                                Text(
-                                    text = title,
-                                    fontSize = 22.sp,
-                                    fontWeight = FontWeight.Bold,
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis,
-                                )
+                                Column() {
+                                    SelectionContainer() {
+                                        Text(
+                                            text = title,
+                                            fontSize = 22.sp,
+                                            fontWeight = FontWeight.Bold,
+                                            maxLines = 1,
+                                            overflow = TextOverflow.Ellipsis,
+                                        )
+                                    }
+                                }
                                 IconButton(
                                     onClick = { /*TODO*/ },
                                     modifier = Modifier
@@ -125,7 +134,9 @@ fun DisplayApod(
                                         contentAlignment = Alignment.Center
                                     ) {
                                         CircularProgressIndicator(
-                                            modifier = Modifier.size(64.dp)
+                                            modifier = Modifier
+                                                .size(64.dp)
+                                                .padding(24.dp)
                                         )
                                     }
                                 } else {
@@ -164,14 +175,16 @@ fun DisplayApod(
                                 text = "Description",
                                 fontWeight = FontWeight.Bold,
                             )
-                            Text(
-                                modifier = Modifier
-                                    .padding(top = 6.dp, bottom = 16.dp)
-                                    .alpha(0.8f),
-                                text = description,
-                                fontSize = 14.sp,
-                                lineHeight = 18.sp
-                            )
+                            SelectionContainer() {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(top = 6.dp, bottom = 16.dp)
+                                        .alpha(0.8f),
+                                    text = description,
+                                    fontSize = 14.sp,
+                                    lineHeight = 18.sp
+                                )
+                            }
                         }
                     }
                 }
